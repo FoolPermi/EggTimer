5c09e2b0e97942e6f3ddb3c736d85bb721d2542e
FoolPermi <foolpermi@gmail.com>
2019-02-17 21:13:58 +0800

    Add Egg Timer

---
+++ EggTimer/Model/EggTimer.swift
@@ -0,0 +1,83 @@
+//
+//  EggTimer.swift
+//  EggTimer
+//
+//  Created by Permi on 2019/2/17.
+//  Copyright © 2019 Permi. All rights reserved.
+//
+
+import Foundation
+
+protocol EggTimerProtocol {
+    func timeRemainningOnTimer(_ timer: EggTimer, timeRemaining: TimeInterval)
+    func timerHasFinished(_ timer: EggTimer)
+}
+
+class EggTimer {
+    var timer: Timer? = nil
+    var startTime: Date?
+    var duration: TimeInterval = 360
+    var elapsedTime: TimeInterval = 0
+    var delegate: EggTimerProtocol?
+    
+    var isStopped: Bool {
+        return timer == nil && elapsedTime == 0
+    }
+    
+    var isPaused: Bool {
+        return timer == nil && elapsedTime > 0
+    }
+    
+    @objc dynamic func timerAction() {
+        guard let startTime = startTime else{
+            return
+        }
+        elapsedTime = -startTime.timeIntervalSinceNow
+        let secondsRemaining = (duration - elapsedTime).rounded()
+        if secondsRemaining <= 0 {
+            resetTimer()
+            delegate?.timerHasFinished(self)
+        } else {
+            delegate?.timeRemainningOnTimer(self, timeRemaining: secondsRemaining)
+        }
+    }
+    
+    // MARK: - Timer Operations
+    
+    func startTimer() {
+        startTime = Date()
+        elapsedTime = 0
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+        timerAction()
+    }
+    
+    func resumeTimer() {
+        startTime = Date(timeIntervalSinceNow: -elapsedTime)
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+        timerAction()
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+        
+        startTime = nil
+        duration = 360
+        elapsedTime = 0
+        
+        timerAction()
+    }
+}
---
+++ EggTimer/View Controllers/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Permi on 2019/2/2.
+//  Copyright © 2019 Permi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    @IBOutlet weak var timeTextField: NSTextField!
+    @IBOutlet weak var eggImageView:NSImageView!
+    
+    @IBOutlet weak var startButton: NSButton!
+    
+    @IBOutlet weak var stopButton: NSButton!
+    
+    @IBOutlet weak var resetButton: NSButton!
+    
+    var eggTimer = EggTimer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        eggTimer.delegate = self
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+        if eggTimer.isPaused {
+            eggTimer.resumeTimer()
+        } else {
+            eggTimer.duration = 360
+            eggTimer.startTimer()
+        }
+    }
+    @IBAction func stopButtonClicked(_ sender: Any) {
+        eggTimer.stopTimer()
+    }
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        eggTimer.resetTimer()
+        updateDisplay(for: 360)
+    }
+    
+    // MARK:- NSMenu Action
+    @IBAction func startTimerMenuItemSelected(_ sender: NSMenuItem) {
+        startButtonClicked(sender)
+    }
+    @IBAction func stopTimerMenuItemSelected(_ sender: NSMenuItem) {
+        stopButtonClicked(sender)
+    }
+    @IBAction func resetTimerMenuItemSelected(_ sender: NSMenuItem) {
+        resetButtonClicked(sender)
+    }
+}
+
+extension ViewController: EggTimerProtocol {
+    
+    func timeRemainningOnTimer(_ timer: EggTimer, timeRemaining: TimeInterval) {
+        updateDisplay(for: timeRemaining)
+    }
+    
+    func timerHasFinished(_ timer: EggTimer) {
+        updateDisplay(for: 0)
+    }
+}
+
+extension ViewController {
+    // MARK: -Display
+    func updateDisplay(for timeRemaining: TimeInterval) {
+        timeTextField.stringValue = textToDisplay(for: timeRemaining)
+        eggImageView.image = imageToDisplay(for: timeRemaining)
+    }
+    
+    private func textToDisplay(for timeRemaining: TimeInterval) -> String{
+        if timeRemaining == 0 {
+            return "Done"
+        }
+        let minutesRemaining = floor(timeRemaining / 60)
+        let secondRemaining = timeRemaining - (minutesRemaining * 60)
+        
+        let secondsDisplay = String(format: "%02d", Int(secondRemaining))
+        let timeRemaingDisplay = "\(Int(minutesRemaining)):\(secondsDisplay)"
+        
+        return timeRemaingDisplay
+    }
+    
+    private func imageToDisplay(for timeRemaing: TimeInterval) -> NSImage?{
+        let percentageComplete = 100 - (timeRemaing / 360 * 100)
+        
+        if eggTimer.isStopped {
+            let stopppedImageName = (timeRemaing == 0) ? "100":"stopped"
+            return NSImage(named: .init(stopppedImageName))
+        }
+        let imageName: String
+        switch percentageComplete {
+        case 0..<25:
+            imageName = "0"
+        case 25..<50:
+            imageName = "25"
+        case 50..<75:
+            imageName = "50"
+        case 75..<100:
+            imageName = "75"
+        default:
+            imageName = "100"
+        }
+        return NSImage(named: .init(imageName))
+    }
+}
